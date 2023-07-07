c3dfef5dd682da7ad6cbae3d8c24f486629974b0
Christhopher Lion <christhopherleao@icloud.com>
2023-07-08 00:56:15 +0200

    scaffold new weather module for Prodapt
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

---
+++ apps/maelstorm/src/app/ElementsStack/Forecast.view.tsx
@@ -0,0 +1,78 @@
+import { useWeatherElements } from '@elements/services';
+import _ from 'lodash';
+import { Suspense, useMemo } from 'react';
+import { SafeAreaView } from 'react-native';
+import ErrorBoundary from 'react-native-error-boundary';
+import { Stack, Text, YStack, H1, ScrollView, Image, View } from 'tamagui';
+
+import type { RootStackScreenProps } from '../navigation/navigation.types';
+
+export function ForecastView(props: RootStackScreenProps<'Forecast'>) {
+  const { data } = useWeatherElements();
+  const { cityId } = props.route.params;
+  const forecast = useMemo(() => data[cityId], [data, cityId]);
+
+
+  return (
+    <Suspense fallback={<Text>Data is on the way</Text>}>
+      <ErrorBoundary FallbackComponent={() => <Text>Something went wrong</Text>}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Stack flex={1} space={20}>
+            <Image
+              height="100%"
+              position='absolute'
+              source={{ uri: forecast[0].city.picture, width: 200, height: 500 }}
+              width="100%"
+            />
+            <View
+              backgroundColor={'#000'}
+              height="100%"
+              opacity={0.6}
+              position='absolute'
+              width="100%"
+            />
+            <H1
+              color={'$colorAlt'}
+              fontSize={36}
+              fontWeight={'200'}
+              lineHeight={36 * 1.2}
+              textAlign='center'
+              textShadowColor={'#000'}
+              textShadowRadius={18}>
+              {cityId}
+            </H1>
+
+            <ScrollView
+              contentContainerStyle={{
+                alignItems: 'flex-start',
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                width: '100%',
+              }}
+              horizontal>
+              {_.sortBy(forecast, d => d.date)
+                .map(d => (
+                  <YStack
+                    key={d.date}
+                    margin={4}>
+                    <H1
+                      color={'$colorAlt'}
+                      flex={1}
+                      fontWeight={'900'}
+                      key={d.date}>
+                      {`${d.temp}°`}
+                    </H1>
+                  </YStack>
+                ))}
+            </ScrollView>
+          </Stack>
+        </SafeAreaView>
+      </ErrorBoundary>
+    </Suspense>
+  );
+}
+
+export default {
+  name: 'Forecast',
+  component: ForecastView,
+};
---
+++ apps/maelstorm/src/app/ElementsStack/List.view.tsx
@@ -0,0 +1,40 @@
+import { ListItem } from '@elements/components';
+import { useWeatherElements } from '@elements/services';
+import { Suspense } from 'react';
+import { ActivityIndicator, FlatList } from 'react-native';
+import ErrorBoundary from 'react-native-error-boundary';
+import { Text } from 'tamagui';
+
+import type { RootStackScreenProps } from '../navigation/navigation.types';
+
+export function ListView(props: RootStackScreenProps<'List'>) {
+  const { navigation } = props;
+  const { data, refetch, isRefetching } = useWeatherElements();
+
+  // Unfortunately offline-mode kinda ruins suspense mode
+  if (!data) return <ActivityIndicator />
+
+  return (
+    <Suspense fallback={<Text>Data is on the way</Text>}>
+      <ErrorBoundary FallbackComponent={() => <Text>Something went wrong</Text>}>
+        <FlatList
+          data={Object.keys(data).sort()}
+          onRefresh={refetch}
+          refreshing={isRefetching}
+          renderItem={(d) => (
+            <ListItem
+              key={d.index}
+              onPress={() => navigation.push('Forecast', { cityId: d.item })}
+              title={d.item}
+            />
+          )}
+        />
+      </ErrorBoundary>
+    </Suspense>
+  );
+}
+
+export default {
+  name: 'List',
+  component: ListView,
+};
