3cc3aeb2824def6fea95e88529b8b8dfb9b1c464
Christhopher Lion <christhopherleao@icloud.com>
2023-07-17 17:13:41 +0200

    update and use ENV vars
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>
    romve offline view
    
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

--- libs/services/src/network.ts
+++ libs/services/src/network.ts
@@ -1,16 +1,12 @@
 import axios from 'axios';
-
-// We all know what they say about API keys and env-vars.. but i hardcoded. yep.
-const baseURL = process.env.API_URL || 'https://api.openweathermap.org';
-const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY || 'fb74eb4077417a230cd10f54a92dec87';
-
+import Config from 'react-native-config';
 
 /**
  * Setup and axios instance to fly network requests.
  * its good for its neat api, familiarity and useful interceptors.
 */
 export const network = axios.create({
-  baseURL,
+  baseURL: Config.API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -20,7 +16,7 @@ export const network = axios.create({
 network.interceptors.request.use((config) => {
   const params = {
     ...config.params,
-    appid: OPEN_WEATHER_API_KEY,
+    appid: Config.OPEN_WEATHER_API_KEY,
   };
 
   return {
